afbb8c5f520690571a98d68660b42cf2c2c340e5
derekm41 <derekm41@users.noreply.github.com>
2022-10-21 15:23:53 -0700

    generate csv function

--- comments.py
+++ comments.py
@@ -2,11 +2,15 @@ from urllib import response
 from googleapiclient.discovery import build
 import pandas as pd
 import credentials
+import csv
+
 
 api_key = credentials.api_key
 video_id = 'jV8IyoATXwA'
 
 youtube = build('youtube', 'v3', developerKey=api_key)
+
+comment_list = []
 
 #Function to get video comments
 def get_video_comments(youtube, vidId):
@@ -17,17 +21,30 @@ def get_video_comments(youtube, vidId):
 
     response = request.execute()
     total_results = response['pageInfo']['totalResults']
+    print(total_results)
     counter = 0
-    comment_list = []
+    
+
     for comment in range(counter, total_results):
         author = response['items'][counter]['snippet']['topLevelComment']['snippet']['authorDisplayName']
-        text = comment_text = response['items'][counter]['snippet']['topLevelComment']['snippet']['textDisplay']
-        comment = author + ' : ' + text
+        text = response['items'][counter]['snippet']['topLevelComment']['snippet']['textDisplay']
+        comment = author + ':' + text
         comment_list.append(comment)
         counter = counter + 1
         
     print(comment_list)
+    print(len(comment_list))
 
-    # return response
+def generate_csv():
+     #Some characters in comments like pics come through weird. This can be remedied with encoding=''. Different utf produce different results.
+    with open('comments.csv', 'w', newline='', encoding='utf-8-sig') as csvfile:
+        thewriter = csv.writer(csvfile)
+
+        for comment in comment_list:
+            seperate = comment.split(':')
+            author = seperate[0]
+            cText = seperate[1]
+            thewriter.writerow([author, cText])
 
 get_video_comments(youtube, video_id)
+generate_csv()
