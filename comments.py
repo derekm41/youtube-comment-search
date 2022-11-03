df812d6d7e50ffb48ba460dd360b4feeba99c8e8
derekm41 <derekm41@users.noreply.github.com>
2022-11-03 16:32:06 -0700

    extract time and format into csv for filtering

--- comments.py
+++ comments.py
@@ -4,36 +4,52 @@ import pandas as pd
 import credentials
 import csv
 
-
+#global variables
 api_key = credentials.api_key
-video_id = 'jV8IyoATXwA'
+video_id = 'e6cV0X6gQg0'
+# 'jV8IyoATXwA'
+comment_list = []
 
 youtube = build('youtube', 'v3', developerKey=api_key)
 
-comment_list = []
-
 #Function to get video comments
-def get_video_comments(youtube, vidId):
+def get_video_comments(youtube, vidId, token=''):
+
     request = youtube.commentThreads().list(
         part='snippet',
-        videoId=vidId
+        videoId=vidId,
+        maxResults=100,
+        pageToken=token
     )
-
     response = request.execute()
-    total_results = response['pageInfo']['totalResults']
-    print(total_results)
-    counter = 0
-    
+    #For loop variables
+    for item in response['items']:
+        Tauthor = item['snippet']['topLevelComment']['snippet']['authorDisplayName']
+        Ttext = item['snippet']['topLevelComment']['snippet']['textDisplay']
+        hour, min = extractHrsMins(Ttext)
+        comment_list.append([Tauthor, hour, min])
+    # Watch for recurrsion limits
+    if "nextPageToken" in response:
+        return get_video_comments(youtube, vidId, response['nextPageToken'])
 
-    for comment in range(counter, total_results):
-        author = response['items'][counter]['snippet']['topLevelComment']['snippet']['authorDisplayName']
-        text = response['items'][counter]['snippet']['topLevelComment']['snippet']['textDisplay']
-        comment = author + ':' + text
-        comment_list.append(comment)
-        counter = counter + 1
-        
-    print(comment_list)
-    print(len(comment_list))
+def extractHrsMins(commentText):
+    time = [char for char in commentText if char.isdigit()]
+    time_format = ""
+    time_length = len(time)
+    if time_length == 3:
+        hour = time[0]
+        min = time[1] + time[2]
+        time_format = time[0] + 'hrs ' + time[1] + time[2] + 'mins'
+    elif time_length == 1: 
+        hour = time[0]
+        min = 0
+        time_format = time[0] + 'hrs 0mins'
+    else:
+        hour = 0
+        min = 0
+        time_format = 'NA'
+    return hour, min
+
 
 def generate_csv():
      #Some characters in comments like pics come through weird. This can be remedied with encoding=''. Different utf produce different results.
@@ -41,10 +57,10 @@ def generate_csv():
         thewriter = csv.writer(csvfile)
 
         for comment in comment_list:
-            seperate = comment.split(':')
-            author = seperate[0]
-            cText = seperate[1]
-            thewriter.writerow([author, cText])
+            author = comment[0]
+            hour = comment[1]
+            min = comment[2]
+            thewriter.writerow([author, hour, min])
 
 get_video_comments(youtube, video_id)
 generate_csv()
